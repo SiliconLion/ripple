123dfc6fca939768b3d4567975b7d8cc531441b7
Silica Lion <silicalion@gmail.com>
2026-07-02 18:44:06 +0000

    makes this a CLI app

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,29 @@ use crate::link::Link;
 use crate::simple_impl::{HtmlChecker, HtmlSelector, ShuffleStrat};
 use crate::utils::*;
 
+use std::env;
+
 fn main() -> Result<(), AnyErr> {
-    let root_page = String::from("https://www.talesofaredclayrambler.com/episodes?year=2017");
+    let args: Vec<String> = env::args().collect();
+    // first arg is always name of program, so len_of_our_args + 1
+    if args.len() < 3 {
+        bail!("expected at least two arguments: target_node_count and root_page");
+    }
+
+    let target_node_count = match args[1].parse::<usize>() {
+        Ok(count) => count,
+        Err(_) => bail!("first argument is expected to be a positive number"),
+    };
+    if target_node_count <= 0 {
+        bail!("first argument is expected to be a positive number");
+    }
+
+    let root_link = match Link::new(&args[2]) {
+        Ok(link) => link,
+        Err(_) => bail!("Second argument must be a valid URL (including schema)"),
+    };
+
+    // let root_page = String::from("https://www.talesofaredclayrambler.com/episodes?year=2017");
     // let root_page = String::from("https://www.goodmorningandgoodnight.com/");
     // let root_page = String::from("https://www.scrapethissite.com/pages/");
     // let root_page = String::from("https://bored.com/");
@@ -29,13 +50,13 @@ fn main() -> Result<(), AnyErr> {
     // let root_page = String::from("https://curlie.org");
 
     let mut app = Application::new(
-        Box::new(ShuffleStrat::new(100, 500)),
+        Box::new(ShuffleStrat::new(100, target_node_count)), //ToDo: think more about how to pick max at once
         Box::new(HtmlSelector::new()),
         Box::new(HtmlChecker::new()),
         Box::new(HashData::new()),
     );
 
-    app.start(Link::new(&root_page)?)?;
+    app.start(root_link)?;
 
     println!("Complete");
     Ok(())
